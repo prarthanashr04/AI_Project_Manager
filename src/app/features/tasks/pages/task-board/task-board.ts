9c774f4b2d11b9b8425e25908a4f70de1353cc36
Prarthana Sharma <prarthanashr04@gmail.com>
2026-05-19 17:20:53 +0530

    new task form added

--- src/app/features/tasks/pages/task-board/task-board.ts
+++ src/app/features/tasks/pages/task-board/task-board.ts
@@ -1,5 +1,6 @@
 import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { TaskService } from '../../services/task-service';
 import { DragDropModule, CdkDragDrop } from '@angular/cdk/drag-drop';
 import { TaskStatus } from '../../models/task.model';
@@ -8,14 +9,14 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-task-board',
   standalone: true,
-  imports: [CommonModule, DragDropModule],
+  imports: [CommonModule, DragDropModule, FormsModule],
   templateUrl: './task-board.html',
   styleUrl: './task-board.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TaskBoard implements OnInit {
   projectId!: number;
-
+  newTaskTitle = '';
   constructor(
     private route: ActivatedRoute,
     public taskService: TaskService
@@ -32,5 +33,22 @@ export class TaskBoard implements OnInit {
     const task = event.item.data;
 
     this.taskService.updateTaskStatus(task.id, status);
+  }
+
+  createTask() {
+
+    if (!this.newTaskTitle.trim()) return;
+
+    this.taskService.addTask({
+      id: Date.now(),
+
+      title: this.newTaskTitle,
+
+      status: 'TODO',
+
+      projectId: this.projectId
+    });
+
+    this.newTaskTitle = '';
   }
 }
